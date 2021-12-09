c03c0633c422d2515d8b7005c806fd1a64d7bcf8
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-12-09 14:56:41 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+import string
+
+def passwordGenerator(vTamanhoSenha):
+    '''
+    Função para Geração de senhas aleatórias utilizando Letras (Caixa alta e Baixa) + Número + Caracteres especiais
+
+    Args:
+        vlength (str): Tamanho da senha que será gerada
+
+    Return:
+        (str) Senha gerada
+    '''
+    vLower = string.ascii_lowercase #LISTAR TODAS AS LETRAS EM CAIXA BAIXA
+    vUpper = string.ascii_uppercase #LISTAR TODAS AS LETRAS EM CAIXA ALTA
+    vDigits = string.digits  #LISTAR TODOS OS NUMEROS
+    vSymbols = string.punctuation #LISTAR TODOS OS CARACTERES ESPECIAIS 
+    vAll = vLower + vSymbols + vUpper + vDigits #JUNÇÃO DE TODOS OS TIPOS DE CARACTERES
+    # print(f'lower {lower}')
+    # print(f'upper {upper}')
+    # print(f'digits {digits}')
+    # print(f'symbols {symbols}')
+    # print(f'all {all}')
+    
+    vLength = vTamanhoSenha #TAMANHO DA SENHA QUE SERÁ GERADA
+    password = "".join(random.sample(vAll, vLength))
+    return password
+
+
+print (f'Senha: {passwordGenerator(15)}')
+
